e72f8e49aa91e4d52061ed0d75eef3a2bdd4e0bd
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-09-28 10:29:41 +0530

    FirstNonRepeatElement program implemented

---
+++ java8features/src/com/example/java8freProQ/Java8Features.java
@@ -0,0 +1,35 @@
+package com.example.java8freProQ;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Java8Features {
+   public static void main(String[] args) {
+      String input = "my name is vaibhav yeotikar";
+//      Map<String, Long> map = (Map)Arrays.stream(input.split("")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+//      System.out.println(map);
+
+      String firstNonRepeatElement=Arrays.stream(input.split(""))
+              .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new,Collectors.counting()))
+              .entrySet()
+              .stream()
+              .filter(x->x.getValue()==1)
+              .findFirst().get().getKey();
+      System.out.println(firstNonRepeatElement);
+
+      int[] numbers={5,9,8,11,2,21};
+
+      Integer secondHighest=Arrays.stream(numbers).boxed()
+              .sorted(Comparator.reverseOrder())
+              .skip(1)
+              .findFirst()
+              .get();
+      System.out.println(secondHighest);
+
+
+   }
+}
