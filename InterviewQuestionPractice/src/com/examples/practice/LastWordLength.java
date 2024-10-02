fdbba07623ff0caa0ac9c39da32f1c72bddbba37
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-10-02 17:47:11 +0530

    last word length program implemented

---
+++ InterviewQuestionPractice/src/com/examples/practice/LastWordLength.java
@@ -0,0 +1,21 @@
+package com.examples.practice;
+
+public class LastWordLength {
+    public static int LengthOfLastWord(String s){
+        String str=s.trim();
+        int count=0;
+        for (int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args){
+        String s="My Name is Vaibhav ";
+        System.out.println(LengthOfLastWord(s));
+    }
+}
