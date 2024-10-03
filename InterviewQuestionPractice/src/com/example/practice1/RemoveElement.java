a427cdf263fcf8090ac7da43e9ada31ebdf1486a
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-10-03 18:41:06 +0530

    remove element  program practiced

--- InterviewQuestionPractice/src/com/example/practice1/RemoveElement.java
+++ InterviewQuestionPractice/src/com/example/practice1/RemoveElement.java
@@ -3,12 +3,19 @@ package com.example.practice1;
 public class RemoveElement {
     static int removeElement(int[] nums,int val){
        int count=0;
-       
+       for(int i=0;i<nums.length-1;i++){
+           if(nums[i]!=val){
+               //here 2 will be eleminating and printing remaining numbers
+               nums[count]=nums[i];
+               count++;
+           }
+       }
+       return count;
     }
 
     public static void main(String args[]){
         int [] nums={0,1,2,2,3,0,4,2};
-        int val=2;
+        int val=0;//measns here occurence should be removed and print remainig count
         System.out.println(removeElement(nums,val));
     }
 }
