b96cb5d7471c4b73e5ba8803ed5b2a179c3cab15
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-10-04 13:36:07 +0530

    remove duplicated progem practiced

--- InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted.java
+++ InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted.java
@@ -2,18 +2,17 @@ package com.example.practice1;
 
 public class RemoveDuplicatesFromSorted {
     public static int removeDuplicates(int[] nums) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-                if(i< nums.length-1 && nums[i]==nums[i+1]){
-                    continue;
-                }
-                else{
-                    nums[count]=nums[i];
-                    count++;
-                }
-
-        }
-        return count;
+       int count=0;
+       for(int i=0;i<nums.length;i++){
+           if(i< nums.length-1 && nums[i]==nums[i+1]){
+               continue;
+           }
+           else{
+               nums[count]=nums[i];
+               count++;
+           }
+       }
+       return count;
     }
 
     public static void main(String args[]){
--- InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted2.java
+++ InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted2.java
@@ -1,19 +1,18 @@
 package com.example.practice1;
 
 public class RemoveDuplicatesFromSorted2 {
-    public static void main(String args[]){
-        int [] nums={0,0,1,1,1,1,2,3,3};
+    public static void main(String args[]) {
+        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
         System.out.println(removeDuplicates(nums));
     }
 
-    public  static int removeDuplicates(int[] nums){
-        int i=0;
-
-        for(int n:nums){
-            if(i<2 || n!=nums[i-2]){
-                nums[i++]=n;
+    public static int removeDuplicates(int[] nums) {
+        int i = 0;
+        for (int n:nums) {
+            if (i < 2 && n != nums[i - 1]) {
+                nums[i++] = n;
             }
         }
-        return i;
+        return i++;
     }
 }
