854bd7e119cd1285f86ce6db271b72f05ce78158
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-10-04 14:03:32 +0530

    majority element  progem practiced

--- InterviewQuestionPractice/src/com/example/practice1/MajorityElement.java
+++ InterviewQuestionPractice/src/com/example/practice1/MajorityElement.java
@@ -4,17 +4,18 @@ public class MajorityElement {
     public static int MajorityElement(int[] nums){
         int count=0;
         int candidate=0;
-
-        for(int num:nums){
-            if(count==0){
-                candidate=num;
-            }
-            count+=(num==candidate)?1:-1;
-        }
-        return candidate;
+         for(int n:nums){
+             //The current candidate has no "votes" left, or all previously accumulated votes have been canceled out by other elements.
+             //Since the votes for the current candidate have neutralized, it's time to choose a new candidate
+             if(count==0){
+                 candidate=n;
+             }
+             count=count+((n==candidate)?1:-1);
+         }
+         return candidate;
     }
     public static void main(String args[]){
-        int[] nums={2,2,1,1,1,2,2,1,3};
+        int[] nums={2,2,1,1,1,2,2,3};
         System.out.println(MajorityElement(nums));
     }
 
--- InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted2.java
+++ InterviewQuestionPractice/src/com/example/practice1/RemoveDuplicatesFromSorted2.java
@@ -2,14 +2,14 @@ package com.example.practice1;
 
 public class RemoveDuplicatesFromSorted2 {
     public static void main(String args[]) {
-        int[] nums = {0, 0, 1, 1, 1, 1, 2, 3, 3};
+        int[] nums = {0, 0, 1, 1, 2, 3, 3,4};
         System.out.println(removeDuplicates(nums));
     }
 
     public static int removeDuplicates(int[] nums) {
         int i = 0;
         for (int n:nums) {
-            if (i < 2 && n != nums[i - 1]) {
+            if (i < 2 || n != nums[i - 1]) {
                 nums[i++] = n;
             }
         }
