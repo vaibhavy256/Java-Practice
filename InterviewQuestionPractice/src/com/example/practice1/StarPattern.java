97af57023416d554057c1a7d28ec982bc2974aaf
Vaibhav Yeotikar <92702730+vaibhavy256@users.noreply.github.com>
2024-12-31 19:11:15 +0530

    star pattern program implemented

---
+++ InterviewQuestionPractice/src/com/example/practice1/StarPattern.java
@@ -0,0 +1,12 @@
+package com.example.practice1;
+
+public class StarPattern {
+    public static void main(String[] args) {
+        for(int i=0;i<=4;i++){
+            System.out.println("");
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+        }
+    }
+}
